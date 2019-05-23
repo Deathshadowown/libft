ac5952b7773b13f675ad8ff7cb13d6244de7fef1
Shane Van Niekerk <svan-nie@c3r6s4.wethinkcode.co.za>
2019-05-23 14:00:10 +0200

    recreating functions atoi, strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svan-nie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 13:29:51 by svan-nie          #+#    #+#             */
+/*   Updated: 2019/05/23 13:41:08 by svan-nie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t count;
+
+	count = 0;
+	while (count < n + 1 && s1[count] != '\0' && s2[count] != '\0')
+	{
+		if (count < n && s1[count] != s2[count])
+			return (s1[count] - s2[count]);
+		count++;
+	}
+	return (s1[count] - s2[count]);
+}
